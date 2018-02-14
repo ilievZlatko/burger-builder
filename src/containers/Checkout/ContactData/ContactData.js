3e4f9dfee5ee8a2d52f5eca7cd3ba99452f8292a
zlatkoIliev <iliev.zlatko@gmail.com>
2018-02-14 18:31:53 +0200

    update styles

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -18,18 +18,10 @@ class ContactData extends Component {
 			<div className={classes.ContactData}>
 				<h4>Enter your Contact Data</h4>
 				<form>
-					<input type="text" name="name" placeholder="Your Name" />
-					<input type="email" name="email" placeholder="Your Mail" />
-					<input
-						type="text"
-						name="street"
-						placeholder="Your street"
-					/>
-					<input
-						type="text"
-						name="postal"
-						placeholder="Postal code"
-					/>
+					<input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+					<input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
+					<input className={classes.Input} type="text" name="street" placeholder="Your street" />
+					<input className={classes.Input} type="text" name="postal" placeholder="Postal code" />
 					<Button btnType="Success">Order</Button>
 				</form>
 			</div>
