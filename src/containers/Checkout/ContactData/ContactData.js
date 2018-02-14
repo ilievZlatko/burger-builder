73fcfc5d6203e37d34d80076bc7ecef0ad5eadc4
zlatkoIliev <iliev.zlatko@gmail.com>
2018-02-14 17:50:53 +0200

    update checkout page

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+import Button from '../../../components/UI/Button/Button';
+import classes from './ContactData.css';
+
+class ContactData extends Component {
+	state = {
+		name: '',
+		email: '',
+		address: {
+			street: '',
+			postalCode: '',
+		},
+	};
+
+	render() {
+		return (
+			<div className={classes.ContactData}>
+				<h4>Enter your Contact Data</h4>
+				<form>
+					<input type="text" name="name" placeholder="Your Name" />
+					<input type="email" name="email" placeholder="Your Mail" />
+					<input
+						type="text"
+						name="street"
+						placeholder="Your street"
+					/>
+					<input
+						type="text"
+						name="postal"
+						placeholder="Postal code"
+					/>
+					<Button btnType="Success">Order</Button>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default ContactData;
